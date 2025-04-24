d8154ecf08f4720545e416e15cac85a6200eb49a
marcosarias21 <marcosariasolaiz@gmail.com>
2025-04-24 09:25:38 -0300

    Initialize project with Vite and Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="flex h-dvh items-center justify-center">
+      <p>In progress</p>
+    </div>
+  )
+}
+
+export default App

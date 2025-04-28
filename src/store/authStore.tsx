7298ca25e989c8cb463191bfb81d599dda5e3118
marcosarias21 <marcosariasolaiz@gmail.com>
2025-04-28 10:42:19 -0300

    Login function w/token added

--- src/store/authStore.tsx
+++ src/store/authStore.tsx
@@ -4,16 +4,23 @@ import { User } from '../types'
 
 interface loginStore {
   user: User | null
+  token: string | null
   getUser: (newUser: User) => void
+  getToken: (newToken: string) => void
 }
 
 export const useAuthStore = create<loginStore>()(
   persist(
     (set) => ({
       user: null,
+      token: null,
       getUser: (newUser) =>
         set({
           user: newUser,
+        }),
+      getToken: (newToken) =>
+        set({
+          token: newToken,
         }),
     }),
     {

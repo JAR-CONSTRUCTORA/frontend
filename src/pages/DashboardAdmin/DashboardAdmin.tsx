074ea953dd6bd9d1b91ba22e4577baae7c51e989
Juanivene <168385555+Juanivene@users.noreply.github.com>
2025-05-05 16:36:40 -0300

    mejoro la separacion y el tamaño de los inputs, cambio divs por aside

--- src/pages/DashboardAdmin/DashboardAdmin.tsx
+++ src/pages/DashboardAdmin/DashboardAdmin.tsx
@@ -1,28 +1,30 @@
 import { ModalAdminTask } from '@/components/ModalAdminTask'
 import { Sidebar } from '@/components/Siderbar'
-import { useAuthStore } from '@/store/authStore'
+// import { useAuthStore } from '@/store/authStore'
 
 const DashboardAdmin = () => {
-  const { user } = useAuthStore()
+  // const { user } = useAuthStore() no se usa porque no se necesita en este momento
   return (
-    <div className="mx-2 grid h-dvh grid-cols-12 gap-10 text-gray-200">
-      <div className="col-span-2 my-2 rounded-xl border-2 border-white/20 bg-[#212121]">
+    <div className="mx-2 grid h-dvh grid-cols-12 gap-6 text-gray-200">
+      <aside className="col-span-2 my-2 rounded-xl border border-white/20 bg-[#212121]">
         <Sidebar />
-      </div>
-      <div className="col-span-10 my-2 rounded-xl border-2 border-white/20 bg-[#212121]">
-        <div className="mt-10 ml-2">
-          <h2 className="text-xl font-medium">Create Task</h2>
-        </div>
-        <div className="mx-10 grid grid-cols-4 gap-2 gap-y-5">
-          <div className="cursor-pointer rounded border-1 border-white/20">
+      </aside>
+
+      <main className="col-span-10 my-2 rounded-xl border border-white/20 bg-[#212121] p-6">
+        <header className="mb-6">
+          <h2 className="text-2xl font-semibold">Create Task</h2>
+        </header>
+
+        <section className="grid grid-cols-4 gap-4">
+          <div className="cursor-pointer rounded-lg border border-white/20 p-4">
             <ModalAdminTask />
           </div>
-          <div className="rounded border-1 border-white/20">Task</div>
-          <div>Task</div>
-          <div>Task</div>
-          <div>Task</div>
-        </div>
-      </div>
+          <div className="rounded-lg border border-white/20 p-4">Task</div>
+          <div className="rounded-lg border border-white/20 p-4">Task</div>
+          <div className="rounded-lg border border-white/20 p-4">Task</div>
+          <div className="rounded-lg border border-white/20 p-4">Task</div>
+        </section>
+      </main>
     </div>
   )
 }

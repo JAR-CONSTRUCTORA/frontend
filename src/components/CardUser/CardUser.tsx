991f33c5c3682f74c020942a57c6b369f4bf5d4e
marcosarias21 <marcosariasolaiz@gmail.com>
2025-06-05 10:16:38 -0300

    Cambio de estilo en las cards de usuarios

--- src/components/CardUser/CardUser.tsx
+++ src/components/CardUser/CardUser.tsx
@@ -1,4 +1,5 @@
 import { User } from '@/types'
+import { User2 } from 'lucide-react'
 import React from 'react'
 
 type Prop = User
@@ -11,21 +12,25 @@ const CardUser: React.FC<Prop> = ({
 }) => {
   return (
     <div className="group relative rounded-xl border border-white/10 bg-[#2a2a2a] p-4 transition-shadow hover:bg-gray-800 hover:shadow-xl">
-      <div>
-        <div className="flex flex-col gap-1 font-medium text-gray-100">
-          <h3 className="mb-5 text-lg font-semibold">
-            Empleado: {firstName} {lastName}
-          </h3>
-
-          <p className="line-clamp-2 break-words">{username}</p>
+      <div className="flex h-full gap-5">
+        <div className="flex items-center">
+          <span className="rounded-full border-1 p-5">
+            <User2 />
+          </span>
+        </div>
+        <div className="flex h-full flex-col justify-center gap-2 font-medium text-gray-100">
+          <div>
+            <h3 className="text-lg font-semibold">
+              {firstName} {lastName}
+            </h3>
+            <span>Cargo: {role}</span>
+          </div>
+          <p className="line-clamp-2 break-words">
+            Nombre de usuario: {username}
+          </p>
           <p>Contraseña: {password}</p>
-          <p>Cargo:{role}</p>
         </div>
       </div>
-      <div className="mt-4 w-full"></div>
-      <span className="absolute top-full left-1/2 z-10 mt-2 hidden -translate-x-1/2 rounded bg-gray-800 px-2 py-1 text-sm text-white group-hover:block">
-        Ver detalle
-      </span>
     </div>
   )
 }

74547bb01acba83142b5423e2e00b38b371e6e47
marcosarias21 <marcosariasolaiz@gmail.com>
2025-06-09 11:10:32 -0300

    Se creo el filtrador de usuarios y sus alerts

--- src/components/Filters/FilterUser.tsx
+++ src/components/Filters/FilterUser.tsx
@@ -1,55 +1,28 @@
-import axios from 'axios'
 import { useState } from 'react'
-import { toast } from 'sonner'
 import { User } from '@/types'
 
 interface FiltrosProps {
   setUsers: (users: User[]) => void
+  searchUser: (
+    prompt: string,
+    setIsSearching: React.Dispatch<React.SetStateAction<boolean>>,
+  ) => void
+  getUsers: () => void
 }
 
-const FilterUser = ({ setUsers }: FiltrosProps) => {
-  const [incidencia, setIncidencia] = useState('')
-  const [isSearching, setIsSearching] = useState(false)
+const FilterUser = ({ searchUser, getUsers }: FiltrosProps) => {
+  const [prompt, setPrompt] = useState<string>('')
+  const [isSearching, setIsSearching] = useState<boolean>(false)
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
-
-    setIsSearching(true)
-    try {
-      if (incidencia) {
-        const response = await axios.get(
-          `http://localhost:8000/task/incidencia/${incidencia}`,
-        )
-
-        if (response.data.incidenceFounded) {
-          setUsers([response.data.incidenceFounded])
-        } else {
-          setUsers([])
-          toast.error(response.data.message || 'No se encontró la incidencia')
-        }
-      } else {
-        await resetFilters()
-      }
-    } catch (error) {
-      console.error('Error searching tasks:', error)
-      setUsers([])
-      toast.error('Error al buscar tareas')
-    } finally {
-      setIsSearching(false)
-    }
+    searchUser(prompt, setIsSearching)
   }
 
   const resetFilters = async () => {
-    setIncidencia('')
-
-    try {
-      const tasksResp = await axios.get('http://localhost:8000/task')
-      setUsers(tasksResp.data.allTasks)
-      toast.success('Filtros limpiados correctamente')
-    } catch (error) {
-      console.error('Error fetching all tasks:', error)
-      toast.error('Error al cargar todas las tareas')
-    }
+    setPrompt('')
+    setIsSearching(false)
+    getUsers()
   }
 
   return (
@@ -67,15 +40,15 @@ const FilterUser = ({ setUsers }: FiltrosProps) => {
             htmlFor="incidencia"
             className="mb-1 block text-sm text-gray-300"
           >
-            Número de incidencia
+            Nombre completo de usuario
           </label>
           <input
             id="incidencia"
             type="text"
-            placeholder="Número de incidencia"
+            placeholder="Nombre y apellido de usuario"
             className="w-full rounded-md border border-gray-600 bg-gray-700 p-2 text-white focus:border-[#1e3a5f] focus:outline-none"
-            value={incidencia}
-            onChange={(e) => setIncidencia(e.target.value)}
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
           />
         </div>
       </div>
